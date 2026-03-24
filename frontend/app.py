594b8cbe130d6f6a89165e6a707ecfcdca237d58
Tanmay0108 <tanmaypotnurwar0108@gmail.com>
2026-03-24 19:16:59 +0530

    frontend created !

--- frontend/app.py
+++ frontend/app.py
@@ -9,12 +9,12 @@ load_dotenv()
 API_URL = os.getenv("API_URL")
 
 st.set_page_config(
-    page_title="MedBuddy.ML",   
+    page_title="Medvora.ML",   
     page_icon="⚕️",
     layout="centered"
 )
 
-st.title("⚕️ MedBuddy.ML")
+st.title("⚕️ Medvora.ML")
 st.write("Heart Disease Risk Predictor")
 
 st.subheader("Enter patient details and click **Predict**")
